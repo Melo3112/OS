b2985080894a343214713dbbf28a1291bfa19da6
Melo3112 <marlonandres20@gmail.com>
2025-05-02 09:52:21 -0500

    api

---
+++ api/database.py
@@ -0,0 +1,19 @@
+import sqlite3
+import os
+
+#create and connet database
+conn = sqlite3.connect('os.db')
+cur = conn.cursor()
+
+sql_database = '''
+    CREATE TABLE IF NOT EXISTS data(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    temp_value REAL NOT NULL,
+    hum_value REAL NULL,
+    status BOOLEAN DEFAULT true, 
+    created_at TIMESTAMP DEFAULT (datetime('now','localtime'))
+    )
+'''    
+cur.execute(sql_database)
+conn.commit()
+conn.close()
